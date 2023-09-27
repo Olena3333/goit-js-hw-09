f3d38594ad0ddd91dd70a58a5d22fe902213fb87
Olena <helensmart2323@gmail.com>
2023-09-27 14:38:26 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
 
+const body = document.body;
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+
+let id = null;
+
+function changeBackground() {
+  body.style.background = getRandomHexColor();  
+}
+const onStartClick = () => {
+    let id = setInterval(changeBackground(), 1000); 
+    if (!id === null) {
+        startBtn.style = "disabled"; 
+    }
+}
+
+const onStopClick = () => {
+    clearInterval(id);
+}
+
+startBtn.addEventListener('click', onStartClick);
+startBtn.addEventListener('click', onStopClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
