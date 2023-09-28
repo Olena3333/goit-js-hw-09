263bd2fdf94b6db0a0ab49c1f3ac333e06611aba
Olena <helensmart2323@gmail.com>
2023-09-28 13:01:11 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,28 +1,37 @@
-
 const body = document.body;
-const startBtn = document.querySelector("[data-start]");
-const stopBtn = document.querySelector("[data-stop]");
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
 
 let id = null;
 
+stopBtn.disabled = true;
+
 function changeBackground() {
-  body.style.background = getRandomHexColor();  
+  body.style.background = getRandomHexColor();
 }
 
-const onStartClick = () => {
-    let id = setInterval(changeBackground(), 1000); 
-    if (id === null) {
-        startBtn.style = "disabled"; 
-    }
+function disabledBtn(a, b) {
+  startBtn.disabled = a;
+  stopBtn.disabled = b;
 }
 
-const onStopClick = () => {
-    clearInterval(id);
+function onStartClick() {
+  id = setInterval(() => {
+    changeBackground();
+    disabledBtn(true, false);
+  }, 1000);
 }
 
-startBtn.addEventListener('click', onStartClick);
-startBtn.addEventListener('click', onStopClick);
+function onStopClick() {
+  clearInterval(id);
+  disabledBtn(false, true);
+}
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
